2281b92160685d4c2ffdfb5adfde007f5bb30f60
Hai Le <evanle2004@gmail.com>
2024-10-19 13:36:01 -0700

    Added a template folder. Added basic HTML structure. Added a function that uses render_template()

--- website/backend.py
+++ website/backend.py
@@ -1,13 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 # Initialize the app
 app = Flask(__name__)
 
 # Define a route
 @app.route('/')
-def home():
-    return "Hello, Flask!"
+@app.route('/<userName>')
+def home(userName="Guest"):
+    return render_template('home.html', name=userName)
 
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
+
+    
