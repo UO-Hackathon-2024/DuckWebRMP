ed8a28fca3f0389272c400bb198f9ad4fd9d80f6
Kai_Hogan <kaidavishogan@gmail.com>
2024-10-19 15:40:48 -0700

    Descriptive message about the changes

--- website/backend.py
+++ website/backend.py
@@ -10,6 +10,11 @@ app = Flask(__name__)
 def home(userName="Guest"):
     return render_template('home.html', name=userName)
 
+courses = ['CS 102', 'CS 110', 'CS 111', 'CS 122', 'CS 210', 'CS 212', 'CS 314', 'CS 322', 'CS 332', 'CS 402', 'CS 407', 'CS 410', 
+           'CS 415', 'CS 420', 'CS 422', 'CS 425', 'CS 431', 'CS 432', 'CS 443', 'CS 451', 'CS 471', 'CS 507', 'CS 510', 'CS 520', 
+           'CS 522', 'CS 531', 'CS 532', 'CS 543', 'CS 551', 'CS 571', 'CS 607', 'CS 610', 'CS 640', 'CS 633']
+     
+
 # Run the app
 if __name__ == '__main__':
     app.run(debug=True)
