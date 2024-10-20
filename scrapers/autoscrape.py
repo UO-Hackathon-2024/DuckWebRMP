c025a63b1b74d63b69811787f412d60056882e3f
Daniel Asiamah <danasiamah003@gmail.com>
2024-10-20 01:05:15 -0700

    auto scraper? ahve not tested

---
+++ scrapers/autoscrape.py
@@ -0,0 +1,24 @@
+import time
+import schedule
+
+from dw_to_database import main as dw_main
+from rmp_scraper import main as rmp_main
+
+
+def dw_scraper():
+    print('Running Duckweb Scraper')
+    dw_main()
+    
+    rmp_scraper()
+
+
+def rmp_scraper():
+    print('Running Rate My Professor Scraper')
+    rmp_main()
+
+
+schedule.every(24).hours.do(dw_scraper)
+
+while True:
+    schedule.run_pending()
+    time.sleep(10)
